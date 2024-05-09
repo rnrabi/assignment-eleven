4a11ec1e936302e7ff9d728a295b0622a22fb9b9
rnrabi <rnrabi913@gmail.com>
2024-05-09 21:47:35 +0600

    error page is ok

--- src/pages/error/ErrorPage.jsx
+++ src/pages/error/ErrorPage.jsx
@@ -1,9 +1,17 @@
+import { Link } from "react-router-dom";
+import error from '../../assets/error.jpg'
 
 
 const ErrorPage = () => {
     return (
         <div>
-            <h2>Thsis is error page</h2>
+            <section className="flex items-center h-full sm:p-16 dark:bg-gray-50 dark:text-gray-800">
+                <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8 space-y-8 text-center sm:max-w-md">
+                    <img src={error} alt="" />
+                    <p className="text-3xl">Opss!!!!!! Nothing Found.....</p>
+                    <Link to='/' className="px-8 py-3 font-semibold rounded dark:bg-violet-600 dark:text-gray-50 btn btn-outline">Back to homepage</Link>
+                </div>
+            </section>
         </div>
     );
 };
