1fe7eaa929d8db5a12c06b2422f3868ffe0b597b
rnrabi <rnrabi913@gmail.com>
2024-05-09 20:59:19 +0600

    navber setup is ok

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navber from "../components/Navber";
 
 
 const MainLayout = () => {
     return (
         <div className="w-11/12 mx-auto">
-            <h2>This is main layout</h2>
+            <Navber></Navber>
             <Outlet />
         </div>
     );
