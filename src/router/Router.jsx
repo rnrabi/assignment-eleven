6f56732e8584aeadb741d386c6e3d0d168e75521
rnrabi <rnrabi913@gmail.com>
2024-05-10 10:58:53 +0600

    login page ok

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,6 +3,7 @@ import MainLayout from "../layout/MainLayout";
 import Home from "../pages/home/Home";
 import ErrorPage from "../pages/error/ErrorPage";
 import Register from "../pages/register/Register";
+import Login from "../pages/login/Login";
 
 export const router = createBrowserRouter([
     {
@@ -17,6 +18,10 @@ export const router = createBrowserRouter([
         {
             path:'/register',
             element:<Register></Register>
+        },
+        {
+          path:'/login',
+          element:<Login></Login>
         }
       ]
     },
